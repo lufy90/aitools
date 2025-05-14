7e60e45d74ba1f2df5f475a857cf52fe12f6e4b5
Lufei <lufei@uniontech.com>
2025-05-14 17:09:40 +0800

    add wkdata

---
+++ wkdata/config.py
@@ -0,0 +1,11 @@
+
+endpoints = [
+        ## from https://www.mediawiki.org/wiki/API:Main_page
+        "https://www.mediawiki.org/w/api.php",
+        "https://meta.wikimedia.org/w/api.php",
+        "https://en.wikipedia.org/w/api.php",
+        "https://nl.wikipedia.org/w/api.php",
+        "https://commons.wikimedia.org/w/api.php",
+        "https://test.wikipedia.org/w/api.php",
+        "https://en.wikipedia.org/w/rest.php/v1/page/{word}",
+        ]
